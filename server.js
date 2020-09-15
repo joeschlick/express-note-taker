cb6cb9e2b37052bc949fbd173a963d62329a14dc
slowdive <schlickjoe@gmail.com>
2020-09-15 00:30:17 -0700

    changed api routes in app.use to /api

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 //Dependencies
 const express = require("express");
-const fs = require("fs");
+const apiRoutes = require("./routes/apiRoutes")
+const htmlRoutes = require("./routes/htmlRoutes")
 
 const app = express();
 const PORT = process.env.PORT || 4040;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
-// require("./routes/apiRoutes")(app);
-// require("./routes/htmlRoutes")(app);
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
