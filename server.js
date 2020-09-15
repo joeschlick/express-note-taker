61ec54ff257a046d94435fc14eeadaf91d5cf9f3
slowdive <schlickjoe@gmail.com>
2020-09-14 18:06:08 -0700

    added code to start up server

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
 //Dependencies
 const express = require("express");
-const fs 
+const fs = require("fs");
+
+const app = express();
+const PORT = process.env.PORT || 4040;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// require("./routes/apiRoutes")(app);
+// require("./routes/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
